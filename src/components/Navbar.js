f8f4d856bda807a3402037de5bdc07dfb08e66ff
talhaty <talhayounas0348@gmail.com>
2023-04-27 15:36:01 +0500

    patch1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand href="#" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
